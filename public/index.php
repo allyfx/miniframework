24abc1339d8584aed0415429975c3d70a9802f73
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-09-19 19:18:50 -0300

    Controllers added

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 	//Require autoload to test
 	require_once "../vendor/autoload.php";
 	
-	// In CMD, go to the project's public directory with cd command and run php -S localhost:<port> to run the server.
-
-	echo 'Hello World!';
+	$route = new \App\Route;
 
 ?>
