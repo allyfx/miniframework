8b8a1879145da2b70f6b0f002da19d6b48d9bc2b
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-08 17:40:41 -0300

    Last commit

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,13 +2,24 @@
 
 	namespace App\Controllers;
 	use MF\Controller\Action;
+	use MF\Model\Container;
 
 	class IndexController extends Action {
 		public function index() {
+			$produto = Container::getModel('Produto');
+			$produtos = $produto->getProdutos();
+
+			$this->view->dados = $produtos;
+
 			$this->render("index", "layout1");
 		}
 
 		public function sobreNos() {
+			$info = Container::getModel('Info');
+
+			$infos = $info->getInfos();
+			$this->view->dados = $infos;
+
 			$this->render("sobreNos", "layout1");
 		}
 	}
